c72d1b140b15bfa8e94f8c98ea3eae60e3b9b1e2
Marcos Coelho <mviniciusca@gmail.com>
2022-09-12 19:18:19 -0300

    moved title to initial point

--- letscode/src/exercicios/src/Calculadora.java
+++ letscode/src/exercicios/src/Calculadora.java
@@ -1,26 +1,23 @@
 package exercicios.src;
 
+/**
+ * Escreva um programa que informa a tabuada de multiplicação de um número
+ * informado pelo
+ * usuário (entre 1-10). Exemplo:
+ * Entre com um número de 1 a 10:
+ * 1
+ * A tabuada de 1 é:
+ * 1 x 1 = 1
+ * 1 x 2 = 2
+ * 1 x 3 = 3
+ */
+
 public class Calculadora {
     public static void main(String[] args) {
-
-        /**
-         * Escreva um programa que informa a tabuada de multiplicação de um número
-         * informado pelo
-         * usuário (entre 1-10). Exemplo:
-         * Entre com um número de 1 a 10:
-         * 1
-         * A tabuada de 1 é:
-         * 1 x 1 = 1
-         * 1 x 2 = 2
-         * 1 x 3 = 3
-         */
-
-        calculadora(15);
-
+        calculadora(3);
     }
 
     public static void calculadora(int number) {
-
         if (number > 0 && number <= 10) {
             System.out.println("A Tabuada do " + number + " é: ");
             System.out.println("___________________");
---
+++ letscode/src/exercicios/src/ConversorTemperatura.java
@@ -0,0 +1,22 @@
+package exercicios.src;
+
+/**
+ * Escreva um programa que converte temperaturas de Celsius para Fahrenheit.
+ * Fórmula (0 °C ×
+ * 9/5) + 32 = 32 °F. Exemplo:
+ * Entre com uma temperatura em Celsius para saber seu equivalente em
+ * Fahrenheit:
+ * 22
+ * O equivalente a 22 C é 71,6 F.
+ */
+
+public class ConversorTemperatura {
+    public static void main(String[] args) {
+
+    }
+
+    public static void conversor(int temperatura) {
+
+    }
+
+}
