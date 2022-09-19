10dd4678321314f16c1b0d6eeb08088aa640eb72
Marcos Coelho <mviniciusca@gmail.com>
2022-09-19 04:25:54 -0300

    add new exercises

---
+++ letscode/src/exercicios/beecrowd/SomaSimples.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * Leia dois valores inteiros. A seguir, calcule o produto entre estes dois
+ * valores e atribua esta operação à variável PROD. A seguir mostre a variável
+ * PROD com mensagem correspondente.
+ * 
+ * Entrada
+ * O arquivo de entrada contém 2 valores inteiros.
+ * 
+ * Saída
+ * Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um
+ * espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim
+ * de linha após o produto, caso contrário seu programa apresentará a mensagem:
+ * “Presentation Error”.
+ */
+
+public class SomaSimples {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int soma = A + B;
+
+        System.out.printf("SOMA = %s\n", soma);
+
+    }
+
+}
