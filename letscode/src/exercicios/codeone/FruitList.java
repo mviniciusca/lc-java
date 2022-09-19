02aefb42731518a35f0dff827c24e60b56525ea0
Marcos Coelho <mviniciusca@gmail.com>
2022-09-19 16:54:19 -0300

    corrections beecrowd folder

--- letscode/src/exercicios/codeone/FruitList.java
+++ letscode/src/exercicios/codeone/FruitList.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class FruitList {
     public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Hortifruti! Qual fruta deseja comprar?");
 
+        Scanner sc = new Scanner(System.in);
+        String frutaDesejada = sc.nextLine();
+        String[] frutasPromocao = { "uva", "maçã", "banana", "pera", "morango" };
+        boolean promocao = false;
+
+        for (String fruta : frutasPromocao) {
+            if (fruta.equalsIgnoreCase(frutaDesejada)) {
+                promocao = true;
+                break;
+            }
+        }
+
+        if (promocao) {
+            System.out.println("A fruta está em promoção!");
+        } else {
+            System.out.println("Hoje a fruta não está em promoção!");
+        }
+
+        sc.close();
     }
 
 }
