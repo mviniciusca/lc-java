1f0eb316a83997d47ed0a1d7821fc02e93fda689
Marcos Coelho <mviniciusca@gmail.com>
2022-09-21 01:30:19 -0300

    solved number pair

--- letscode/src/exercicios/codeone/NumberPair.java
+++ letscode/src/exercicios/codeone/NumberPair.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class NumberPair {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        do {
+            if (x < y) {
+                System.out.println("Ordem Crescente");
+            } else {
+                System.out.println("Ordem Decrescente");
+            }
+            x = sc.nextInt();
+            y = sc.nextInt();
+        } while (x != y);
+        sc.close();
     }
 
 }
